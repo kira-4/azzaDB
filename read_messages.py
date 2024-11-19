8d1652977afde4268e41b826c6cf0d8fd934978d
root <root@v2202402217964257753.quicksrv.de>
2024-11-19 02:51:53 -0800

    batman

---
+++ read_messages.py
@@ -0,0 +1,26 @@
+import asyncio
+from hydrogram import Client
+
+
+api_hash = "164c0237d988d53ad1538adba88196ff"
+api_id = 20655834
+
+bot = Client("ret_mes", api_id=api_id, api_hash=api_hash)
+
+chat_id = -1001869042104
+
+
+async def get_history(chat):
+    # chat_info = await bot.get_chat(chat)
+    # print(f"chat info {chat_info}")
+    async for message in bot.get_chat_history(chat, limit=15):
+        if message.text:
+            print(message)
+
+
+async def main():
+    await bot.start()
+    await get_history(chat_id)
+    await bot.stop()
+
+asyncio.run(main())
